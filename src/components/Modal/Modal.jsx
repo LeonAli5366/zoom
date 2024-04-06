3e1e0c96aa8ea880e02dbcd7f99a14ad8482ea3d
Biplob Hossain <biplob17h@gmail.com>
2024-04-06 12:51:36 -0700

    update

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,34 +1,89 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
+import { useContext } from "react";
 import { RxCross2 } from "react-icons/rx";
+import { AuthContext } from "../../Contextapi/UserContext";
+import { toast } from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+
 const Modal = ({ d, setActive }) => {
+  const { classTitle, classTeacher, _id } = d;
+  const { user } = useContext(AuthContext);
+  const { name, email, photo } = user;
+  const navigate = useNavigate();
+
+  const handleStudentLogInInClass = (event) => {
+    event.preventDefault();
+    const password = event.target.password.value;
+
+    if (!user?.email) {
+      return navigate("/login");
+    }
+
+    const data = {
+      name,
+      email,
+      photo,
+      classId: _id,
+      password,
+    };
+
+    console.log(data);
+
+    fetch("http://localhost:5000/api/v1/class/addStudent", {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.status === "success") {
+          toast.success("class login successful");
+          navigate("/");
+        } else if (data.status === "fail") {
+          toast.error(data.message);
+        } else {
+          toast.error("something went wrong");
+        }
+      });
+  };
   return (
-    <div className="w-[500px] bg-white shadow-lg rounded p-10 flex flex-col text-dark gap-5 relative">
+    <form
+      onSubmit={handleStudentLogInInClass}
+      className="w-[500px] bg-white shadow-lg rounded p-10 flex flex-col text-dark gap-5 relative"
+    >
       <button
         className="size-8 bg-slate-50 absolute right-3 top-3 rounded-full flex items-center justify-center"
-        onClick={() => setActive('')}
-      ><RxCross2 size={25} className="opacity-50"/></button>
-      <div className="flex items-center gap-5">
-        <div className="size-16 rounded-full bg-slate-50"></div>
-        <span className="font-medium text-lg">{d.name}</span>
+        onClick={() => setActive("")}
+      >
+        <RxCross2 size={25} className="opacity-50" />
+      </button>
+      <div className="flex flex-col">
+        {/* <div className="size-16 rounded-full bg-slate-50">{}</div> */}
+        <span className="font-medium text-lg">Title : {classTitle}</span>
+        <span className="font-medium text-lg">Teacher : {classTeacher}</span>
       </div>
-      <h1 className="text-xl">
-        The Jewish Positive-Psychology Approach to Building a Solid Marriage.
-      </h1>
+      <h1 className="text-xl">{d.classDesc}</h1>
       <div className="w-full flex flex-col gap-1">
         <label htmlFor="password" className="font-medium text-sm">
           Password
         </label>
         <input
           type="password"
-          name=""
+          name="password"
           id="password"
           className="w-full py-2 border-none outline-none pl-2 rounded"
         />
       </div>
-      <button className="bg-darkblue py-2 text-white font-medium rounded transition-all duration-300 hover:scale-105">
+      <button
+        type="submit"
+        className="bg-darkblue py-2 text-white font-medium rounded transition-all duration-300 hover:scale-105"
+      >
         Join
       </button>
-    </div>
+    </form>
   );
 };
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
-import { useState } from 'react';
-import SearchBar from '../../components/SearchBar/SearchBar';
-import Modal from '../../components/Modal/Modal';
+/* eslint-disable no-unused-vars */
+import { useEffect, useState } from "react";
+import SearchBar from "../../components/SearchBar/SearchBar";
+import Modal from "../../components/Modal/Modal";
 const data = [
   {
     title: "fdhgfh",
@@ -23,26 +24,34 @@ const data = [
     name: "science",
   },
 ];
+
 const Home = () => {
   const [active, setActive] = useState();
-    return (
-        <div className="w-full h-screen p-10 static pt-20">
-        <div className='max-w-[60%] w-full h-full mx-auto flex flex-col gap-5'>
+  const [classes, setClasses] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/api/v1/class/all")
+      .then((res) => res.json())
+      .then((data) => setClasses(data.class));
+  }, []);
+  return (
+    <div className="w-full h-screen p-10 static pt-20">
+      <div className="max-w-[60%] w-full h-full mx-auto flex flex-col gap-5">
         <SearchBar />
         <div className="w-full grid grid-cols-1 gap-3">
-          {data.map((d, i) => (
+          {classes.map((singleClass, i) => (
             <div
-              key={i}
+              key={singleClass._id}
               className="w-full bg-gray-300 px-5 py-2 rounded flex items-center justify-between"
             >
               <div>
                 {active === i && (
                   <div className="w-full h-full flex items-center justify-center absolute top-0 left-0 modal bg-black bg-opacity-30">
-                    <Modal d={d} setActive={setActive} />
+                    <Modal d={singleClass} setActive={setActive} />
                   </div>
                 )}
-                <span className="text-sm font-normal">{d.title}</span>
-                <h1 className="font-medium">{d.name}</h1>
+                <span className=" font-normal"><span className="font-medium">Title : </span>{singleClass.classTitle}</span>
+                <h1 className=""><span className="font-medium">Teacher: </span>{singleClass.classTeacher}</h1>
               </div>
               <div className="flex items-center gap-5">
                 <span className="text-darkblue">Online</span>
@@ -56,9 +65,9 @@ const Home = () => {
             </div>
           ))}
         </div>
-        </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Home;
